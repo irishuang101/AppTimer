8398e892dd732d2cde787d393713dbaaf4873641
irishuang101 <26huangi49@stu.smuhsd.org>
2023-07-18 10:52:13 -0700

    s

--- AppTimer/ContentView.swift
+++ AppTimer/ContentView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timeRemaining = 10
+    @State private var timeIsRunning = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            
+            Text("Timer")
+            
+            if timerIsRunning {
+                Button("Stop timer")
+            }
+            
+            
+        }//VStack
         .padding()
-    }
-}
+    }//Var body
+}//Struct Contentview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
